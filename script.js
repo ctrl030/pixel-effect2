9ff254864daf442d51972d1714228770ce014d1c
ctrl030 <70302284+ctrl030@users.noreply.github.com>
2021-06-05 21:29:04 -0300

    Update script.js

--- script.js
+++ script.js
@@ -10,11 +10,38 @@ image1.src = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAyAAAAHCCAYAAAAXY63I
 image1.addEventListener('load', function() {
   // can get passed up to 5 args:
   // img to show, startX, startY, width, height
-  ctx.drawImage(image1, 0,0);
+  ctx.drawImage(image1, 0,0, canvas.width, canvas.height);
+  const pixels = ctx.getImageData(0,0, canvas.width, canvas.height);
 
+  let particlesArray = [];
+  const numberOfParticles = 5000; 
+  
+  let mappedImage = [];
+  for (let y = 0; y < canvas.height; y++) {
+    let row = [];
+    for (let x = 0; x < canvas.width; x++) {
+      const red = pixels.data[(y * 4 * pixels.width) + ( x * 4)];
+      const green = pixels.data[(y * 4 * pixels.width) + ( x * 4+1)];
+      const blue = pixels.data[(y * 4 * pixels.width) + ( x * 4 +2)]; 
+      const brightness = calculateRelativeBrightness(red,green,blue);
+      const cell = [
+        cellBrightness = brightness,
+      ];
+      row.push(cell); 
+    }
+    mappedImage.push(row);
+  }
+
+  console.log(mappedImage);
+  function calculateRelativeBrightness(red,green,blue){
+    return Math.sqrt(
+      (red * red) * 0.299 +
+      (green * green)  * 0.587 +
+      (blue * blue)  * 0.114
+    )/100;
+  }
+  
   /*
-  const scannedImage = ctx.getImageData(0,0, canvas.width, canvas.height);
-
   console.log(scannedImage);
   const scannedData = scannedImage.data;
   for ( let i = 0; i< scannedData.length; i+=4){
@@ -26,10 +53,7 @@ image1.addEventListener('load', function() {
   };
   scannedImage.data = scannedData;
   ctx.putImageData(scannedImage, 0,0);*/
-
-  let particlesArray = [];
-  const numberOfParticles = 5000; 
-
+  
   class Particle {
     constructor(){
       // x and y starting points depend on the effect of your choice, here "falling rain"
@@ -38,17 +62,40 @@ image1.addEventListener('load', function() {
       this.speed = 0;
       this.velocity = Math.random() * 3.5;
       this.size = Math.random() * 1.5 + 1;
+      this.position1 = Math.floor(this.y);
     }
     update(){
-      this.y+= this.velocity;
+      this.y += this.velocity;
       if (this.y >= canvas.height){
           this.y = 0;
           this.x = Math.random() * canvas.width;
       }
     }
-    
+    draw(){
+      ctx.beginPath();
+      ctx.fillStyle = 'white';
+      ctx.arc(this.x, this.y, this.size, 0, Math.PI *2);
+      ctx.fill();
+    }
   }
-
+  function init(){
+    for (let i=0; i< numberOfParticles; i++){
+      particlesArray.push(new Particle);
+    };
+  }
+  init();
+  function animate(){
+    ctx.drawImage(image1, 0,0, canvas.width, canvas.height);
+    ctx.globalAlpha = 0.05;
+    ctx.fillStyle = 'rgb(0,0,0)';
+    ctx.fillRect(0,0,canvas.width, canvas.height);
+    for (let i = 0; i < particlesArray.length; i++) {
+      particlesArray[i].update();
+      particlesArray[i].draw();
+    }; 
+    requestAnimationFrame(animate);
+  }
+  animate();
 });
  
  
